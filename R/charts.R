49341d6fbe308e23ac1bc8e1d70b3315e27e0623
Sam Hardwick <hardws@Sams-Air.modem>
2024-10-22 10:33:40 +1100

    charts, bibliography

--- R/charts.R
+++ R/charts.R
@@ -49,12 +49,12 @@ gta_1 <- gta %>%
   summarise(count = n()) %>%
   filter(year > 2008, year < 2024) %>% 
   group_by(is_highincome) %>% 
-  mutate(count = cumsum(count))
+  mutate(count = cumsum(count)) %>% 
+  mutate(label = if_else(year == 2019, 
+                         is_highincome, 
+                         NA))
 
 subsidies <- gta_1 %>% 
-  mutate(label = if_else(year == 2023, 
-                         is_highincome, 
-                         NA)) %>%
   ggplot(aes(x = year, y = count, 
              group = is_highincome, colour = is_highincome)) + 
   # add grid lines
@@ -69,7 +69,7 @@ subsidies <- gta_1 %>%
                colour = "grey91", 
                linewidth = 0.6) +
   # add data lines
-  geom_line(linewidth = 1.25) + 
+  geom_line(size = 1) + 
   theme_minimal() + 
   xlab("Year") + 
   ylab("Subsidies since start of 2009") +
@@ -80,18 +80,147 @@ subsidies <- gta_1 %>%
         plot.margin = unit(c(0, 0.4, 0.2, 0.2), units = "cm")) + 
   guides(colour = FALSE) + # hide legend
   # add labels
-  geom_text_repel(aes(label = label),
-                  hjust = 0, 
-                  force = 1, 
-                  direction = "y",
+  geom_text_repel(data = gta_1 %>% filter(is_highincome == "High income"), 
+                  aes(label = label),
+                  hjust = 0.6, 
+                  vjust = -10, 
+                  force = 0, 
                   nudge_x = 0, 
-                  xlim = c(2023, NA),
-                  na.rm = TRUE, 
-                  segment.linetype = "dotted") + 
+                  nudge_y = 0,
+                  fontface = "bold",
+                  segment.color = "transparent", 
+                  na.rm = TRUE) +
+  geom_text_repel(data = gta_1 %>% filter(is_highincome == "Middle or low income"), 
+                  aes(label = label),
+                  hjust = 0.4, 
+                  vjust = 6.5, 
+                  force = 0, 
+                  nudge_x = 0, 
+                  nudge_y = 0,
+                  segment.color = "transparent",
+                  fontface = "bold",
+                  colour = "#E69F00",
+                  na.rm = TRUE) +
   # accessible colour palette
-  scale_colour_manual(values = palette.colors(palette = "R4")) +
+  scale_colour_manual(values = palette.colors(palette = "Okabe-Ito")) +
   scale_x_continuous(expand = c(0, 0), 
-                     limits = c(2009, 2030), 
+                     limits = c(2009, 2024), 
                      breaks = seq(2009, 2023, by = 2)) 
 
-ggsave("output/figures/subsidies.pdf", width = 12, height = 12, units = "cm")
+subsidies
+ggsave("charts/subsidies.pdf", width = 10, height = 10, units = "cm")
+
+# chart 2: instrument types
+
+palette <- c("#abb0d0", rep("#062E8E", 11))
+
+gta_2 <- gta %>% 
+  mutate(year = year(`Inception Date`)) %>% 
+  filter(year < 2024) %>% 
+  select(`Intervention Type`, `Intervention ID`) %>%
+  na.omit() %>%
+  unique() %>% 
+  group_by(`Intervention Type`) %>%
+  summarise(count = n()) %>% 
+  arrange(-count) %>%
+  mutate(rank = row_number()) %>%
+  mutate(`Intervention Type` = if_else(rank > 11, 
+                                       "Other", 
+                                       `Intervention Type`)) %>% 
+  group_by(`Intervention Type`) %>%
+  summarise(count = sum(count)) %>% 
+  arrange(-count) %>% 
+  rename("Intervention type" = `Intervention Type`) %>%
+  mutate(`Intervention type` =
+           factor(`Intervention type`, 
+                  levels = c("Other", 
+                             "Export tax", 
+                             "Public procurement localisation", 
+                             "Financial assistance in foreign market",
+                             "Tax or social insurance relief",
+                             "Anti-dumping",
+                             "Loan guarantee",
+                             "Local content incentive",
+                             "Trade finance",
+                             "Import tariff",
+                             "State loan",
+                             "Financial grant"))) %>% 
+  mutate(label_1 = if_else(count > 6000, 
+                           `Intervention type`, 
+                           NA), 
+         label_2 = if_else(count < 6000, 
+                           `Intervention type`, 
+                           NA))
+
+instruments <- gta_2 %>%
+  ggplot(aes(count, `Intervention type`)) + 
+  theme_minimal() +
+  geom_col(fill = palette) + 
+  labs(x = NULL, y = NULL) +
+  theme(axis.text.y = element_blank()) +
+  geom_text(aes(label = label_1), 
+            hjust = "right", nudge_x = -200,
+            size = 4, colour = "white",
+            fontface = "bold") +
+  geom_text(aes(label = label_2), 
+            hjust = "left", nudge_x = 200,
+            size = 4, colour = "black",
+            fontface = "bold")
+
+instruments
+ggsave("charts/instruments.pdf", width = 10, height = 10, units = "cm")
+
+# chart 3: simulated subsidy welfare
+ssw_data <- read_csv("data/subsidy_welfare.csv")
+
+ssw_1 <- ssw_data %>%
+  head(21) %>%
+  pivot_longer(c(2:3), names_to = "type", values_to = "welfare") %>%
+  mutate(welfare = welfare / 57.824 * 100, 
+         label = if_else(type == "unilateral" &
+                           rate == 0.17, 
+                         "Last foreign firm exits", 
+                         NA),
+         label_1 = if_else(type == "unilateral" &
+                             rate == 0, 
+                           "Unilateral subsidy", 
+                           NA), 
+         label_2 = if_else(type == "symmetric" &
+                             rate == 0, 
+                           "Symmetric subsidy", 
+                           NA)) %>%
+  ggplot(aes(rate, welfare, group = type, colour = type)) +
+  scale_colour_manual(values = c("#298bd1", "#e07222")) +
+  theme_minimal() +
+  geom_hline(yintercept = 100, linetype = "dashed") +
+  geom_line(size = 1) + 
+  xlab("Subsidy value per input") + 
+  ylab("Expected global welfare") + 
+  theme(legend.position = "none") +
+  geom_text_repel(aes(label = label),
+                  hjust = 1.2, 
+                  vjust = 3, 
+                  force = 0, 
+                  na.rm = TRUE, 
+                  segment.linetype = "dotted") +
+  geom_text_repel(aes(label = label_1),
+                  hjust = 0,
+                  nudge_x = 0.003,
+                  vjust = 4.1, 
+                  force = 0, 
+                  na.rm = TRUE, 
+                  fontface = "bold",
+                  segment.colour = "transparent") +
+  geom_text_repel(aes(label = label_2),
+                  hjust = -0.5, 
+                  nudge_x = 0.003,
+                  vjust = -3.3, 
+                  force = 0, 
+                  na.rm = TRUE, 
+                  fontface = "bold",
+                  segment.colour = "transparent") +
+  scale_y_continuous(limits = c(75, 110))
+
+ssw_1
+ggsave("charts/welfare.pdf", width = 10, height = 10, units = "cm")
+
